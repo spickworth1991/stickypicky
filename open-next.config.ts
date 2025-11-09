1a9bae87cefa0bcd0e25c22e366da2cb9e5e91a8
StickyPicky-Laptop <spickworth1991@gmail.com>
2025-11-09 17:09:04 -0500

    delete open

--- open-next.config.ts
+++
@@ -1,8 +0,0 @@
-// open-next.config.ts
-import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-// Optional: enable Next.js Incremental Cache via R2 later
-// import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
-
-export default defineCloudflareConfig({
-  // incrementalCache: r2IncrementalCache, // uncomment after you bind an R2 bucket
-});
