5052422e2c017030205585a8e179552720eec82c
StickyPicky-Laptop <spickworth1991@gmail.com>
2025-11-09 20:30:22 -0500

    'update'

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
-// tailwind.config.ts (or .js)
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
